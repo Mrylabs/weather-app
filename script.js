01c7bef7ceba1014ff4c56ceb99f817b45114046
Mrylabs <mehryt71@gmail.com>
2025-11-06 18:52:11 +0330

    feat: replace API icons with custom emoji design and add day/night backgrounds

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const cityName = document.getElementById("city-name");
 const temperature = document.getElementById("temperature");
 const description = document.getElementById("description");
 const wind = document.getElementById("wind");
+const icon = document.getElementById("weather-icon");
 
 
 searchBtn.addEventListener("click", () => {
@@ -26,7 +27,6 @@ async function getWeather(city) {
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
     );
     const data = await response.json();
-    message.textContent = "";
 
     if (data.cod !== 200) {
       message.textContent = "City not found 😢";
@@ -37,12 +37,48 @@ async function getWeather(city) {
       return;
     }
 
+    message.textContent = "";
+
+    const weatherMain = data.weather[0].main.toLowerCase();
+
+    const now = data.dt;
+    const sunrise = data.sys.sunrise;
+    const sunset = data.sys.sunset;
+    const isDay = now >= sunrise && now < sunset;
+
+    const body = document.body;
+
+    // simple day/night backgrounds
+    if (isDay) {
+      body.style.background = "linear-gradient(to bottom, #87ceeb, #f0f8ff)"; 
+    } else {
+      body.style.background = "linear-gradient(to bottom, #0f2027, #203a43, #2c5364)";
+    }
+    body.style.transition = "background 1s ease";
+
+    // show sun/moon icon
+    const timeIcon = document.getElementById("time-icon");
+    if (timeIcon) timeIcon.textContent = isDay ? "☀️" : "🌙";
+
+
+    let emoji = "🌈";
+    if (weatherMain.includes("clear")) {
+    emoji = isDay ? "☀️" : "🌙";}
+    else if (weatherMain.includes("cloud")) emoji = "☁️";
+    else if (weatherMain.includes("rain")) emoji = "🌧️";
+    else if (weatherMain.includes("snow")) emoji = "❄️";
+    else if (weatherMain.includes("thunder")) emoji = "⚡";
+    else if (weatherMain.includes("mist")) emoji = "🌫️"; 
+
+  
     cityName.textContent = data.name;
     temperature.textContent = `Temperature: ${data.main.temp}°C`;
     description.textContent = data.weather[0].description;
     wind.textContent = `${data.wind.speed} m/s`;
+    icon.textContent = emoji; 
 
   } catch (error) {
+    console.error(error);
     message.textContent = "Something went wrong. Please try again.";
   }
 }
