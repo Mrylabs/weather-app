5d960e5d9223e7f1eed2dc72de3894b087a96d7e
Mrylabs <mehryt71@gmail.com>
2026-02-08 01:38:18 +0330

    Fix port for Render deployment

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import express from "express";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // simple CORS (manual, no package)
 app.use((req, res, next) => {
